c555d7ec084ac5bc4aec82c6f2e61e2cdfc5f9c6
saranshmehra <saranshmehra325@gmail.com>
2026-06-16 03:15:00 +0530

    showed online status using a custom hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
@@ -26,6 +27,13 @@ const Body = () => {
         ?.restaurants
     );
   };
+
+  const onlineStatus = useOnlineStatus();
+  if (!onlineStatus) {
+    return (
+      <h1>Looks like you're offline !! Please check your internet connection</h1>
+    )
+  }
 
   if (listOfRestaurants.length === 0) {
     return <Shimmer />;
